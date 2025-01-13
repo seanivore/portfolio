e2a7234948c305b5bc4f233d765af518924e06b9
seanivore <sean@aug.ltd>
2025-01-12 19:40:12 -0500

    Trying to figure out charts

---
+++ assets/js/charts/ai_ux_tool_consulting_1_data.js
@@ -0,0 +1,9 @@
+const ai_ux_tool_consulting_1_data = [
+
+    { stage: 'Research', progress: 100, complexity: 40, impact: 65 },
+    { stage: 'Analysis', progress: 85, complexity: 60, impact: 75 },
+    { stage: 'Design', progress: 70, complexity: 80, impact: 85 },
+    { stage: 'Testing', progress: 55, complexity: 70, impact: 90 },
+    { stage: 'Implementation', progress: 40, complexity: 50, impact: 95 }
+    
+  ];
